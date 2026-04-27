c4a13b018e40320bcde207fbed1f3bad1b78e692
Nathan S. <nsdsilva@sga.pucminas.br>
2026-04-27 20:39:45 -0300

    Adiciona o exercício do slide 40

---
+++ ALGORITMOS-E-LOGICA/SLIDE 40/slide40.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SIPeriodo1.AlgoritmosELogica.Slide40 {
+    class Slide40 {
+        static void Main(string[] args) {
+
+            int a, b, c;
+            Console.Write("Digite o número para o fatorial: "); //Isso foi adicionado para o usuário saber o que deve ser digitado, já que o programa não tem interface gráfica.
+            a = int.Parse(Console.ReadLine() ?? "0");
+            b = 1;
+
+            for (c = 2; c <= a; c++) {
+                b = b * c;
+            }
+
+            Console.Write(b);
+            Console.ReadKey(); //Após isso aqui o programa já estava funcionando, mas como o exemplo do professor não retornava nada, eu adicionei o comentário na linha 7. 
+
+        }
+    }
+}

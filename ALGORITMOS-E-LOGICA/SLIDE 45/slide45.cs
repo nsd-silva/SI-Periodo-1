8452aef6ad062ad27af362d242639f7b536a36d5
Nathan S. <nsdsilva@sga.pucminas.br>
2026-04-27 21:47:04 -0300

    Adiciona o exemplo do slide 45. É a mesma coisa do slide 44, só apresenta como dá para declarar as váriaveis de mesmo tipo em uma mesma linha, separando apenas por vírgula.

---
+++ ALGORITMOS-E-LOGICA/SLIDE 45/slide45.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace SIPeriodo1.AlgoritmosELogica.Slide45 {
+    class Slide45 {
+
+    //Este é o mesmo exemplo do slide anterior, mas agora as variáveis estão sendo declaradas em uma única linha, separadas por vírgulas.
+        static void Main(string[] args) {
+        int idade, numero;
+        double altura, peso, salario;
+        string nomePai, rua, bairro, dtNasc;
+        bool temCasa;
+        }
+    }
+}

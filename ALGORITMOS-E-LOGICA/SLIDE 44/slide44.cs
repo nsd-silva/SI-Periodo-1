00049cd4f84959e88f621292afed7bad6ac91151
Nathan S. <nsdsilva@sga.pucminas.br>
2026-04-27 21:37:50 -0300

    Adiciona o exemplo do slide 44. É apenas uma declaração simples das diferentes formas de variáveis

---
+++ ALGORITMOS-E-LOGICA/SLIDE 44/slide44.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace SIPeriodo1.AlgoritmosELogica.Slide44 {
+    class Slide44 {
+
+//Este é um código simples demonstrando as diferentes formas de declarar variáveis. 
+        static void Main(string[] args) {
+            int idade; // Declaração de uma variável do tipo inteiro
+            int numero; // Declaração de outra variável do tipo inteiro
+            double altura; // Declaração de uma variável do tipo double para armazenar números com casas decimais
+            double peso; // Declaração de outra variável do tipo double
+            double salario; // Declaração de uma variável do tipo double para armazenar o salário   
+            string nomePai; // Declaração de uma variável do tipo string para armazenar o nome do pai
+            string rua; // Declaração de uma variável do tipo string para armazenar o nome da rua
+            string bairro; // Declaração de uma variável do tipo string para armazenar o nome do bairro
+            string dtNasc; // Declaração de uma variável do tipo string para armazenar a data de nascimento
+            bool temCasa; // Declaração de uma variável do tipo booleano para armazenar se a pessoa tem casa ou não. O tipo booleano só pode armazenar os valores true ou false.           
+        }
+    }
+}

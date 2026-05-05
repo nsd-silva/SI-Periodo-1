0c260ade541402c0605b3ce6b834b08ad7e064d9
Nathan S. <nathansantosds@gmail.com>
2026-05-05 17:47:47 -0300

    Exercício do slide 84 onde exemplifica os diferentes tipos de operações aritmeticas

---
+++ ALGORITMOS-E-LOGICA/SLIDE 84/slide84.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace SIPeriodo1.AlgoritmosELogica.Slide84
+{
+    class Slide84
+    {
+    public static void Main(string[] args)
+        {
+            int a = 5, b = 2, c;
+            int Soma, Subt, Mult, Quociente, Resto;
+            Double DivExata, RaizQuadrada, Quadrado, Cubo, RaizCubica;
+            // Cálculos
+            Soma = a + b;
+            Subt = a - b;
+            Mult = a * b;
+            Quociente = a / b;
+            Resto = a % b;
+            DivExata = 9 / 2.0;
+            RaizQuadrada = Math.Sqrt(36);
+            Quadrado = Math.Pow(a, b);
+            c = b + 1;
+            Cubo = Math.Pow(c, c);
+            RaizCubica = Math.Pow(125, 1/3.0);
+            // Impressão dos resultados
+            Console.WriteLine($"{a} + {b} = {Soma}");
+            Console.WriteLine($"{a} - {b} = {Subt}");
+            Console.WriteLine($"{a} * {b} = {Mult}");
+            Console.WriteLine($"{a} / {b} = {Quociente}");
+            Console.WriteLine($"{a} % {b} = {Resto}");
+            Console.WriteLine($"9 / 2.0 = {DivExata}");
+            Console.WriteLine($"A raiz quadrada de 36 = {RaizQuadrada}");
+            Console.WriteLine($"{a} elevado a {b} = {Quadrado}");
+            Console.WriteLine($"{c} elevado a {c} = {Cubo}");
+            Console.WriteLine($"A raiz cubica de 125 = {RaizCubica}");
+        }   
+    }
+}

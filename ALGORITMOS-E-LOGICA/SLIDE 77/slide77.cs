56c21d6ab1e7f643c780dd962853038bac97bc7a
Nathan S. <nathansantosds@gmail.com>
2026-05-05 17:26:57 -0300

    Exemplo do slide 77 de operadores de incremento/decremento

---
+++ ALGORITMOS-E-LOGICA/SLIDE 77/slide77.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SIPeriodo1.AlgoritmosELogica.Slide77
+{
+    class Slide77
+    {
+        public static void Main(string[] args)
+        {
+            int preinc = 7, posinc = 7, predec = 7, posdec = 7;
+            Console.WriteLine($"preinc = {++preinc}"); //aqui ele incrementa o valor antes de imprimir
+            Console.WriteLine($"posinc = {posinc++}"); //aqui ele imprime o valor e depois incrementa
+            Console.WriteLine($"predec = {--predec}"); //aqui ele decrementa o valor antes de imprimir
+            Console.WriteLine($"posdec = {posdec--}"); //aqui ele imprime o valor e depois decrementa
+            Console.WriteLine("\nREIMPRIMINDO");
+            Console.WriteLine($"preinc = {preinc}"); //aqui ele imprime o valor atualizado do preinc
+            Console.WriteLine($"posinc = {posinc}"); //aqui ele imprime o valor atualizado do posinc
+            Console.WriteLine($"predec = {predec}"); //aqui ele imprime o valor atualizado do predec
+            Console.WriteLine($"posdec = {posdec}"); //aqui ele imprime o valor atualizado do posdec
+        }
+    }
+}  

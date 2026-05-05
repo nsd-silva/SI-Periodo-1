4884bd1a8840dd20f4351074e3c750a63a17c333
Nathan S. <nathansantosds@gmail.com>
2026-05-05 15:54:56 -0300

    Exemplo do slide 71 explicando a entrada, saída e processamento de dados

---
+++ ALGORITMOS-E-LOGICA/SLIDE 71/slide71.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SIPeriodo1.AlgoritmosELogica.Slide71
+{
+    class Slide71
+    {
+     public static void Main(string[] args)
+        {
+            int n1, n2, soma;
+            double media;
+            Console.Write("Digite um número: ");
+            n1 = int.Parse(Console.ReadLine()!);
+            Console.Write("Digite outro número: ");
+            n2 = int.Parse(Console.ReadLine()!);
+            soma = n1 + n2;
+            media = soma / 2.0;
+            Console.WriteLine("Soma:"+soma);
+            Console.WriteLine("Média:"+media);
+        }   
+    }
+}
